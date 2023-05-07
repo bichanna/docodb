e9a3a51ebdc403cc338f990c12b206819fdd26e7
bichanna <nobu.bichanna@gmail.com>
2023-05-07 15:11:32 -0400

    Write a test for the JsonParser

--- src/test/scala/util/json/JsonParserTest.scala
+++ src/test/scala/util/json/JsonParserTest.scala
@@ -1,6 +1,25 @@
 package com.bichanna.docodb
 package util.json
 
-class JsonParserTest {
-
-}
+import util.json.parser.CirceParser
+import org.scalatest.funsuite.AnyFunSuite
+class JsonParserTest extends AnyFunSuite:
+  test("Circe parser test") {
+    val json = """
+    {
+      "str": "Nobu",
+      "num": 17,
+      "list": ["tennis", "piano"],
+      "bool": true,
+      "null": null
+    }
+    """
+    val result = CirceParser.parse(json).getOrElse(JsonNull)
+    assert(result == JsonObject(Map(
+      "str" -> JsonString("Nobu"),
+      "num" -> JsonNumber(17),
+      "list" -> JsonArray(Seq(JsonString("tennis"), JsonString("piano"))),
+      "bool" -> JsonBoolean(true),
+      "null" -> JsonNull,
+    )))
+  }
